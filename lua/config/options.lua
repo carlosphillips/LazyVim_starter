a9d1d46640d1bb9a64d90d4c785d6621b811f3a6
Carlos Phillips <carlos.e.phillips@gmail.com>
2023-10-01 02:36:30 -0400

    add basic options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,6 +5,9 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
 
+-- Window bar
+vim.opt.winbar = "%=%m %f"
+
 -- Indenting
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
@@ -12,3 +15,26 @@ vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 
+vim.opt.wrap = false
+
+-- Backups
+vim.opt.swapfile = false
+vim.opt.backup = false
+
+-- Hilights
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+
+-- Minimum number of screen lines above and below cursor
+vim.opt.scrolloff = 8
+
+-- Open help in a new buffer instead of a vsplit
+vim.api.nvim_create_autocmd("BufWinEnter", {
+  pattern = "*",
+  callback = function(event)
+    if vim.bo[event.buf].filetype == "help" then
+      vim.cmd.only()
+      vim.bo.buflisted = true
+    end
+  end,
+})
