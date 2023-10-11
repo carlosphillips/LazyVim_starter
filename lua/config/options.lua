fca16036a650688665effadeb30a790dfcb8b4f4
Carlos Phillips <carlos.e.phillips@gmail.com>
2023-10-10 21:26:36 -0400

    use treesitter for code folding

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -15,6 +15,11 @@ vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 
+-- Folding
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+vim.opt.foldenable = false
+
 -- Text wrapping
 vim.opt.wrap = false
 
